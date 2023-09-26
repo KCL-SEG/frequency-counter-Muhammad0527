ad45bea9b6d8ca30bc0620bc2ebbb56460016a7d
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-09-26 20:01:16 +0100

    Make function counting the number of times the item represented by the key occurs in items.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if str(item) in frequencies:
+            frequencies[str(item)] = frequencies[str(item)] + 1
+        else:
+            frequencies[str(item)] = 1
     return frequencies
+
+print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
+print(frequencies([100, 'Hello', '100', '100', 100]))
